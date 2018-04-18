5307906b469ac445f74c29a997ac0cbc4d2751b0
Blanche Faivre <blanche.faivre@laposte.net>
2018-04-18 16:01:45 +0200

    Ajout score

--- js/Main.js
+++ js/Main.js
@@ -17,40 +17,6 @@ function my_function(){
 	window.location.reload();
 }
 
-<<<<<<< HEAD
-//********************* INIT ***********************
-function init(){
-	//Chargement
-	console.log("page chargee");
-	
-	//Récupération du contexte graphique
-	canvas = document.querySelector("#myCanvas");
-	ctx = canvas.getContext("2d");
-	var width_canvas = canvas.width;
-	var height_canvas = canvas.height;
-	
-	// Gestion SoundWave
-	onLoadSoundWave();
-	//Ecouteurs de clavier
-	window.onekeydown = traiteKeydown;
-	window.onkeyup = traiteKeyup;
-	
-	//Dessin du joueur
-	joueur = new Voiture(width_canvas / 2,height_canvas,30,30, 'red');
-	joueur.vitesseX = 0;
-	joueur.vitesseY = 5;
-	
-	//canvas.getContext('2d').putImageData(data, 0, 0);
-	
-	voitures.push(joueur);
-	
-	//Demarrage de l'animation
-	requestAnimationFrame(animation);
-}
-  
-=======
->>>>>>> f2451d2bf2b73ece80c70f95ea5066b51c71c262
-
 // main.js
 function init() {
   console.log("page chargee");
@@ -72,6 +38,8 @@ function init() {
   
   joueur = new Voiture(370, 500, 0, 0, 0,'black');
   
+  // Gestion SoundWave
+	onLoadSoundWave();
   // Ecouteurs de clavier
   window.onkeydown = traiteKeydown;
   window.onkeyup = traiteKeyup;
